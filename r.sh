96de09fc39868d1557f5359995a43eef7dea1352
Avishek <50791518+CHAOSandORDER@users.noreply.github.com>
2019-05-20 14:03:00 -0400

    Add files via upload

--- r.sh
+++ r.sh
@@ -2,42 +2,64 @@
 
 
 
-iwconfig
+#iwconfig
 
-echo -n "
+#echo -n "
 
-Enter name of interface . E.g 0 for wlan0mon:"
+#Enter name of interface . E.g 0 for wlan0mon:"
 
-read in
+#read in
 
-if [ $in -eq 0 ]
+#if [ $in -eq 0 ]
 
-then
+#then
 
-	ifconfig wlan0 down
-	iwconfig wlan0 mode managed
-	ifconfig wlan0 up
+#	ifconfig wlan0 down
+#	iwconfig wlan0 mode managed
+#	ifconfig wlan0 up
 	
-fi
+#fi
 
-if [ $in -eq 1 ]
+#if [ $in -eq 1 ]
 
-then
+#then
 
-	ifconfig wlan1 down
-	iwconfig wlan1 mode managed
-	ifconfig wlan1 up
+#	ifconfig wlan1 down
+#	iwconfig wlan1 mode managed
+#	ifconfig wlan1 up
 	
-fi
+#fi
 
-if [ $in -eq 2 ]
+#if [ $in -eq 2 ]
 
-then
+#then
 
-	ifconfig wlan2 down
-	iwconfig wlan2 mode managed
-	ifconfig wlan2 up
+#	ifconfig wlan2 down
+#	iwconfig wlan2 mode managed
+#	ifconfig wlan2 up
 	
-fi
+#fi
 
 service network-manager restart
+
+echo -n "Do you want to aircrack ?Yes(1) or No(2). Enter 1 or 2 :"
+
+read r
+
+if [ $r -eq 1 ] 
+
+then
+	read -p "Enter path to wordlist :" l
+	read -p "Enter bssid :" bs
+	read -p "Enter path to handshake.cap :" c
+	aircrack-ng -w $l -b $bs $c
+
+elif [ $r -eq 2 ]
+
+then
+	exit
+
+else
+
+	exit
+fi
