eb38934def7380d6ba99850d46a279f3d4092bae
Avishek <50791518+CHAOSandORDER@users.noreply.github.com>
2019-07-10 16:42:37 +0530

    Add files via upload

--- r.sh
+++ r.sh
@@ -1,65 +1,85 @@
 #!/bin/bash
 
 
-
-#iwconfig
-
-#echo -n "
-
-#Enter name of interface . E.g 0 for wlan0mon:"
-
-#read in
-
-#if [ $in -eq 0 ]
-
-#then
-
-#	ifconfig wlan0 down
-#	iwconfig wlan0 mode managed
-#	ifconfig wlan0 up
-	
-#fi
-
-#if [ $in -eq 1 ]
-
-#then
-
-#	ifconfig wlan1 down
-#	iwconfig wlan1 mode managed
-#	ifconfig wlan1 up
-	
-#fi
-
-#if [ $in -eq 2 ]
-
-#then
-
-#	ifconfig wlan2 down
-#	iwconfig wlan2 mode managed
-#	ifconfig wlan2 up
-	
-#fi
-
 service network-manager restart
 
 echo -n "Do you want to aircrack ?Yes(1) or No(2). Enter 1 or 2 :"
 
 read r
 
-if [ $r -eq 1 ] 
+if [ $r -eq 1 ]
 
 then
-	read -p "Enter path to wordlist :" l
-	read -p "Enter bssid :" bs
-	read -p "Enter path to handshake.cap :" c
-	aircrack-ng -w $l -b $bs $c
 
-elif [ $r -eq 2 ]
+	echo -n "DO YOU WANT TO INSTALL A CUSTOM WORDLIST CREATION TOOL? Press 1 for YES and 2 for NO :"
+	
+	read y
+
+	if [ $y -eq 1 ]
+
+	then 
+		echo "installing 
+
+   ___   ___ _   _____   _  ______________    _      ______  ___  ___  __   ______________
+  / _ | / _ \ | / / _ | / |/ / ___/ __/ _ \  | | /| / / __ \/ _ \/ _ \/ /  /  _/ __/_  __/
+ / __ |/ // / |/ / __ |/    / /__/ _// // /  | |/ |/ / /_/ / , _/ // / /___/ /_\ \  / /   
+/_/ |_/____/|___/_/ |_/_/|_/\___/___/____/   |__/|__/\____/_/|_/____/____/___/___/ /_/   
+
+					BY AVISHEK "
+		cd /root
+		git clone https://github.com/CHAOSandORDER/Advancedwordlist.git
+		clear
+		echo -n " IF YOU WANT TO RUN THE TOOL ENTER 1 ELSE PRESS 2: "
+		
+		read g
+			
+			if [ $g -eq 1 ]
+				
+			then
+
+				cd /root  
+				cd Advancedwordlist
+				chmod +x run.sh
+				./run.sh
+
+			fi
+
+			# if [ $g -eq 2]
+
+			# then
+
+
+			# fi
+
+	fi
+
+	if [ $y -eq 2 ]
+
+	then
+
+		echo -n "Do you want to run Advancedwordlist? Press 1 to run else press ENTER:"
+		read d
+		if [ d -eq 1 ]
+		then
+			cd /root  
+			cd Advancedwordlist
+			chmod +x run.sh
+			./run.sh
+		fi
+
+		read -p "Enter path to wordlist :" l
+		read -p "Enter bssid :" bs
+		read -p "Enter path to handshake.cap :" c
+		aircrack-ng -w $l -b $bs $c
+	fi
+
+fi 
+
+if [ $r -eq 2 ]
 
 then
+	
 	exit
 
-else
-
-	exit
 fi
+exit
