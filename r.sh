8a84e6eab84f4ce1303a4da520a0f2661b734f0c
Avishek <50791518+CHAOSandORDER@users.noreply.github.com>
2019-07-10 16:57:35 +0530

    Add files via upload

--- r.sh
+++ r.sh
@@ -44,12 +44,17 @@ then
 
 			fi
 
-			# if [ $g -eq 2]
+			if [ $g -eq 2 ]
 
-			# then
+			then
+
+				read -p "Enter path to wordlist :" l
+				read -p "Enter bssid :" bs
+				read -p "Enter path to handshake.cap :" c
+				aircrack-ng -w $l -b $bs $c
 
 
-			# fi
+			fi
 
 	fi
 
